97d0be622d0ad7831bc6bd77bd57df809763cf1e
sungjun1116 <opr99@naver.com>
2024-02-10 17:03:26 +0900

    #공백 추가

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <div class="welcome-message" v-if="isLoggedIn">
-      <p class="welcome-text">
-        안녕하세요, <span class="user-name">{{ user.name }}</span
-        >님!
-      </p>
-      <p class="description">어제는 즐겁게 식물 관리하셨나요?</p>
+      <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
+      <p class="description">어제는 즐겁게 식물 관리하셨나요? </p>
     </div>
-    <div class="brand" v-if="isLoggedIn">LEAFY</div>
-    <NavBar v-if="isLoggedIn"></NavBar>
+    <div class="brand" v-if="isLoggedIn">
+      LEAFY
+    </div>
+    <NavBar v-if="isLoggedIn"></NavBar>    
     <div class="router-view-wrapper">
       <router-view></router-view>
     </div>
-    <footer class="footer" v-if="isLoggedIn">
+    <footer class="footer" v-if="isLoggedIn"> 
       <p>&copy; 2023 Leafy. All rights reserved.</p>
     </footer>
     <BasicPopup :message="popup.message" :status="popup.status" :visible="popup.visible" @close="closePopup" />
@@ -21,7 +20,7 @@
 
 <script>
 import BasicPopup from "@/components/BasicPopup.vue";
-import NavBar from "@/components/NavBar.vue";
+import NavBar from '@/components/NavBar.vue';
 import { mapState } from "vuex";
 
 export default {
@@ -34,15 +33,15 @@ export default {
     isLoggedIn() {
       return !!this.$store.state.user;
     },
-    ...mapState(["popup", "user"]),
+    ...mapState(["popup", "user"])
   },
   data() {
     return {
       showModal: false,
       modalContent: {
-        title: "",
-        body: "",
-        footer: "",
+        title: '',
+        body: '',
+        footer: '',
       },
     };
   },
@@ -52,7 +51,7 @@ export default {
     };
   },
   methods: {
-    closePopup() {
+    closePopup() {      
       this.$store.commit("setPopup", {
         ...this.popup,
         visible: false,
@@ -74,12 +73,12 @@ body {
 }
 
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  position: relative;
+  position: relative; 
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -94,7 +93,7 @@ body {
 .brand {
   font-size: 2.5em;
   font-weight: bold;
-  color: #8fbc8f;
+  color: #8FBC8F;
   padding-top: 30px;
   padding-bottom: 20px;
 }
@@ -124,7 +123,7 @@ body {
 }
 
 .user-name {
-  color: #8fbc8f;
+  color: #8FBC8F;
 }
 
 .description {
@@ -133,7 +132,7 @@ body {
 }
 
 .footer {
-  background-color: #8fbc8f;
+  background-color: #8FBC8F;
   color: white;
   padding: 1rem;
   font-size: 0.9rem;
